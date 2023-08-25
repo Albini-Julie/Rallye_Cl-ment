de331f136e432056f0105716b0df61a132f4cd9d
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-08-25 12:04:42 +0200

    Ajout des routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AccueilView from '../views/Accueil.vue'
+import PiloteView from '../views/Pilote.vue'
+import VoitureView from '../views/Voiture.vue'
+import SponsorsView from '../views/Sponsors.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+    {path: '/', name: 'accueil', component: AccueilView},
+    {path: '/pilote', name: 'pilote', component: PiloteView},
+    {path: '/voiture', name: 'voiture', component: VoitureView},
+    {path: '/sponsors', name: 'sponsors', component: SponsorsView},
   ]
 })
 
