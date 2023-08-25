60681484ff27255adcbad025a44bfe17e015334d
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-08-25 12:04:35 +0200

    Création du svg menu

---
+++ src/components/icons/menu.vue
@@ -0,0 +1,36 @@
+<template>
+    <svg
+  width="27"
+  height="19"
+  viewBox="0 0 27 19"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  style="width: 25px; height: 17.43px;"
+  preserveAspectRatio="none"
+>
+  <path d="M1.35916 1.0567H26.3576H1.35916Z" fill="#333333"></path>
+  <path
+    d="M1.35916 1.0567H26.3576"
+    stroke="#333333"
+    stroke-width="1.032"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+  <path d="M1.35916 9.77078H18.4533H1.35916Z" fill="#333333"></path>
+  <path
+    d="M1.35916 9.77078H18.4533"
+    stroke="#333333"
+    stroke-width="1.032"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+  <path d="M1.35916 18.4849H10.5775H1.35916Z" fill="#333333"></path>
+  <path
+    d="M1.35916 18.4849H10.5775"
+    stroke="#333333"
+    stroke-width="1.032"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+</svg>
+</template>
