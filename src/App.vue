32f823e55ebe4646b07f24b40d00adf31d52e000
Albini-Julie <julie.albini@edu.univ-fcomte.fr>
2023-08-25 16:07:43 +0200

    Suppression des lignes inutiles App

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HeaderOrdi from "./components/HeaderOrdi.vue"
+
 </script>
 
 <template>
-  <body>
-    <HeaderOrdi class="Header-ordi"/>
-  </body>
   <RouterView />
 </template>
-
-<style>
-
-
-</style>
